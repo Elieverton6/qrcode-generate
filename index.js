6945d93be325d366d2e73262527ebfe9e652e47b
swgamerbr406 <swgamerbr406@gmail.com>
2023-03-01 23:30:13 -0300

    feat: adicionei uma condição para verificar se o user digitou, se estiver tudo certo aparecer uma mensagem na tela

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-let confirmButton = document.getElementById("confirm").addEventListener('click', function () {
+let confirmButton = document.getElementById("confirm").addEventListener('click', () => {
 
-    let txtlink = document.getElementById("txtlink");
-    let imgqrcode = document.getElementById("imgqrcode");
+    let txtLink = document.getElementById("txtlink");
+    let imgQrCode = document.getElementById("imgqrcode");
+    let errorMessage = document.getElementById("errormessage");
+    let sucessMessage = document.getElementById("sucessmessage");
 
-    if(txtlink.value.length == 0) {
-        console.log("por favor digite algo");
+    if(txtLink.value.length == 0) {
+        errorMessage.style.display = "flex";
+        sucessMessage.style.display = "none";
+        console.log("Erro!");
     } else {
-        imgqrcode.src = `https://chart.googleapis.com/chart?chs=200x200&cht=qr&chl=${txtlink.value}`
-        console.log("tudo ok!")
+        imgQrCode.src = `https://chart.googleapis.com/chart?chs=200x200&cht=qr&chl=${txtlink.value}`
+        sucessMessage.style.display = "flex";
+        errorMessage.style.display = "none";
+        console.log("Sucesso!")
     }
-
 });
