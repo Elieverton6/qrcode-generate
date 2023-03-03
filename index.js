d79a67e37feffbc635c0a329a1413f1ce36f289d
swgamerbr406 <swgamerbr406@gmail.com>
2023-03-02 21:15:19 -0300

    fix: resolvido bug de quando o user colocar somente espaços no input ele falar que gerou o qrcode.

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ let confirmButton = document.getElementById("confirm").addEventListener('click',
     let errorMessage = document.getElementById("errormessage");
     let sucessMessage = document.getElementById("sucessmessage");
 
-    if(txtLink.value.length == 0) {
+    if(!txtLink.value.trim()) {
+        imgQrCode.src = ``;
         errorMessage.style.display = "flex";
         sucessMessage.style.display = "none";
-        console.log("Erro!");
+        console.log("[ERRO!]");
     } else {
-        imgQrCode.src = `https://chart.googleapis.com/chart?chs=200x200&cht=qr&chl=${txtlink.value}`
+        imgQrCode.src = `https://chart.googleapis.com/chart?chs=200x200&cht=qr&chl=${txtlink.value}`;
         sucessMessage.style.display = "flex";
         errorMessage.style.display = "none";
-        console.log("Sucesso!")
+        console.log("[SUCESSO!]")
     }
 });
